52f50ae03e8f0cb8c827fd6fe44742a880defda4
ojhilt <ojhilt@gmail.com>
2021-05-13 14:35:48 +0100

    another try

--- django_rq/management/commands/rqworker.py
+++ django_rq/management/commands/rqworker.py
@@ -79,7 +79,7 @@ class Command(BaseCommand):
                 'worker_class': options['worker_class'],
                 'queue_class': options['queue_class'],
                 'job_class': options['job_class'],
-                'name': options['name'] if 'name' in options else uuid.uuid4().hex,
+                'name': uuid.uuid4().hex,
                 'default_worker_ttl': options['worker_ttl'],
             }
             w = get_worker(*args, **worker_kwargs)
